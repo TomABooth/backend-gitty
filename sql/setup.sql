5e16cb795004ed6b3eb3b8d5a25920049d3ac84d
Tom Booth <tombeezle@gmail.com>
2022-12-05 14:16:56 -0800

    started controllers and set up

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,6 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users CASCADE;
 
 CREATE TABLE users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -8,4 +8,11 @@ CREATE TABLE users (
   password_hash VARCHAR NOT NULL,
   first_name VARCHAR NOT NULL,
   last_name VARCHAR NOT NULL
+);
+
+CREATE TABLE github_users (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  login TEXT NOT NULL,
+  email TEST,
+  avatar TEXT
 );
