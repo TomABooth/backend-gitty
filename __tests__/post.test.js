be4addc2692a150dc8fb976d6963f45d680c5bbb
Tom Booth <tombeezle@gmail.com>
2022-12-06 11:45:24 -0800

    get posts working

--- __tests__/post.test.js
+++ __tests__/post.test.js
@@ -2,6 +2,7 @@ const setup = require('../data/setup.js');
 const pool = require('../lib/utils/pool.js');
 const request = require('supertest');
 const app = require('../lib/app');
+const Post = require('../lib/models/Post.js');
 
 jest.mock('../lib/services/github');
 
@@ -26,5 +27,19 @@ describe('posts', () => {
       description: newPost.description,
       user_id: newPost.user_id,
     });
+  });
+
+  it('GET /api/v1/posts should return a list of posts if logged in', async () => {
+    const agent = request.agent(app);
+    const user = await agent
+      .get('/api/v1/github/callback?code=42')
+      .redirects(1);
+    const newPost = await Post.insert({
+      description: 'fancy words',
+      user_id: user.body.id,
+    });
+    const res = await agent.get('/api/v1/posts');
+    expect(res.status).toBe(200);
+    expect(res.body[0]).toEqual(newPost);
   });
 });
